b4fec6b5d9fd5566a6ca0dcf585473b84d022182
WolfFire404 <reillyvas9@live.nl>
2017-03-24 23:02:49 +0100

    2e commit

--- Assets/Script/Checkpoint.cs
+++ Assets/Script/Checkpoint.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour {
 
+	public static Vector3 respawnPosition;
+
 	// Use this for initialization
-	void Start () {
-		
+	void OnTriggerEnter(Collider) {
+		if (other.gameObject.CompareTag("Player")
+		Respawn position = transform.position;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }
